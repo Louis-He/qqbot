019ac2793fb6e621524887a00891ce0fbf476cc8
Louis-He <1726110778@qq.com>
2017-08-19 11:07:50 +0800

    add alert searching function

--- index.php
+++ index.php
@@ -37,10 +37,11 @@
 		$tm = "$y$m$d$h$i";
 		$url = "http://image.nmc.cn/product/$dt/RDCP/medium/SEVP_AOC_RDCP_SLDAS_EBREF_AZ9210_L88_PI_{$tm}00000.PNG";
 		echo json_encode(array("type"=>"img", "url"=>$url));
-	} elseif (substr($req, -strlen("温度"))=='温度') {
+	} elseif ((substr($req, -strlen("温度"))=='温度')  || (substr($req, -strlen("气温"))=='气温')) {
 		//气象站温度
 		$sta = str_replace(" ", "", $req);
 		$sta = str_replace("温度", "", $sta);
+		$sta = str_replace("气温", "", $sta);
 		$sta = str_replace("站", "", $sta);
 		
 		$code = file_get_contents("http://weixin.shkeylab-mh.org/weather_forecasts/".urlencode($sta));
@@ -52,7 +53,27 @@
 			$temp = "{$sta}气象站: 当前温度{$temp}℃";
 		}
 		echo json_encode(array("type"=>"text", "msg"=>$temp));
-	} elseif (is_command($req, "!sign")) {
+	} elseif ((substr($msg, 0, strlen("预警"))=='预警') || (substr($msg, 0, strlen("预警机"))=='预警机') || (substr($msg, 0, strlen("信号"))=='信号')){
+		//查询实时预警信号
+		$js = curl("http://182.254.214.114/wxapp/jsondata/fqwarnlist.js");
+		$js = str_replace("var fqwarns=","",$js);
+		$js = str_replace("var warns=","",$js);
+		$js = str_replace("var fqhistorywarns=[]","@",$js);
+		$fq = json_decode(substr($js, 0, strpos($js, "@")), true);
+		$sh = json_decode(substr($js, strpos($js, "@")+1), true);
+		foreach($sh as $warn) {
+			$ret = str_replace("<br>", "", $warn["htmlword"]);
+			$ret = str_replace("<br/>", "", $ret);
+			$ret = substr($ret, 0, strpos($ret, "防御"));
+			$ret = str_replace("发布:", "发布".$warn['name']."预警信号:", $ret);
+			$ret = $warn['yjid']." ".$warn['name'].'预警：'.$ret;
+			//curl_setopt($ch, CURLOPT_URL, "http://localhost:3200/send?type=group&to=$gid&msg=$ret");
+			$ret = urlencode($ret);
+			echo json_encode(array("type"=>"text", "msg"=>$ret));
+			//echo "curl \"http://localhost:3200/send?type=group&to=$gid&msg=$ret\"";
+		}
+	}elseif (is_command($req, "!sign")) {
+		//签到功能
 		$ret = sign($groupid, $uid);
 		if ($ret["success"] == true) {
 			$json = array("type"=>"text", "msg"=>"签到成功, 获得".$ret["addcoin"]."硬币, 现在共有".$ret["coin"]."硬币");
@@ -61,9 +82,11 @@
 		}
 		echo json_encode($json);
 	} elseif (is_command($req, "!coin")) {
+		//查询硬币
 		$json = array("type"=>"text", "msg"=>"您还有".get_coin($uid)."硬币");
 		echo json_encode($json);
 	} else {
+		//智能自动回复
 		$url = "http://www.tuling123.com/openapi/api";
 		$post_data = json_encode(array ("key" => "6380808b6e454953931ea750ea0a4d5b","info" => $req,"userid" => $uid));
 		
